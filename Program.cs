c5cfd6c1dc8275056659dc570de15c4457276093
Ashwani Jhariya <ashwanijhariya07@gmail.com>
2022-06-14 23:41:30 +0530

    Employee is present or Absent

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage computation");
+var Random = new Random();
+int res = Random.Next(0, 2);
+if (res == 0)
+{
+    Console.WriteLine("Absent");
+}
+else
+{
+    Console.WriteLine("Present");
+}
